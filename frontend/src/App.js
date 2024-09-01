323ff0cf7966219a4ce873ccb595d47be1b29cfe
veggiePizza <74165447+veggiePizza@users.noreply.github.com>
2024-09-01 06:13:07 -0500

    Up-to-date

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { Switch, Route } from "react-router-dom";
-import * as sessionActions from "./store/session";
-import Navigation from "./components/Navigation";
-import LandingPage from "./components/LandingPage/LandingPage";
-import LoginFormModal from "./components/LoginFormModal";
-import SignupFormModal from "./components/SignupFormModal";
-
-function App() {
-  const dispatch = useDispatch();
-  const [isLoaded, setIsLoaded] = useState(false);
-  useEffect(() => {
-    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
-  }, [dispatch]);
-
-  return (
-    <>
-      <Navigation isLoaded={isLoaded} />
-      {isLoaded && (
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route path="/login" component={LoginFormModal} />
-          <Route path="/signup" component={SignupFormModal} />
-        </Switch>
-      )}
-    </>
-  );
-}
-
-export default App;
-=======
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Routes, Route } from "react-router-dom";
@@ -89,4 +56,3 @@ function App() {
 }
 
 export default App;
->>>>>>> new
